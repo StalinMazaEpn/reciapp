6ea585f3f1088c670644414ec54146c1a4a57617
Chalo Salvador <chalosalvador@gmail.com>
2018-03-12 22:41:21 -0500

    Corrección en el app.component para lógica de tour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,25 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
-import { TourPage } from '../pages/tour1/tour1';
+//import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tour:any=false;
-  ruta:any="";
-  rootPage:any;
+  rootPage:any = HomePage;
   //rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    if (this.tour) {
-      this.tour=false;
-      this.rootPage=TourPage;
-    }else{
-      this.rootPage=TabsPage;
-    }
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
-
-    
   }
-  
 
-} 
+
+}
 
