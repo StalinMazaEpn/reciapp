060c94d4880d591a8965da419886f00ab6cffe89
pilar1304 <pilar_1304@hotmail.es>
2018-03-21 16:57:59 -0500

    #62 Se reestablecio app.component.ts y tour.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  //pages: Array<{title: string, component: any}>;
-
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    // used for an example of ngFor and navigation
-    /*this.pages = [
-      { title: 'Perfil', component: HomePage },
-      { title: 'Redes Sociales', component: EcoviaPage },
-      { title: 'Tour', component: IntegracionPage },
-      { title: 'Acerca de...', component: OccidentalPage },
-      { title: 'Cerrar Sesión', component: OccidentalPage }
-    ];*/
-
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
